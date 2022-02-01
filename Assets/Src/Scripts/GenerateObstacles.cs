0ab39370a15768e096a2b091ffdd80f335f890a1
Afodia <chloe.bourgeay@epitech.eu>
2022-02-01 15:31:56 +0100

    add obstacle generator and change map0

---
+++ Assets/Src/Scripts/GenerateObstacles.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace YsoCorp
+{
+    public class GenerateObstacles : YCBehaviour
+    {
+        [SerializeField] Vector3 obsSwpanPos;
+        [SerializeField] GameObject emptyObstacles;
+        [SerializeField] Sprite[] shapes;
+        [SerializeField] Color[] colors;
+
+        public void GenerateObstacleFromString(string obstacle, float speed)
+        {
+            GameObject newObj = Instantiate(emptyObstacles, obsSwpanPos, Quaternion.identity);
+            newObj.GetComponentInChildren<Image>().sprite = shapes[obstacle[0]];
+            newObj.GetComponentInChildren<Image>().color = colors[obstacle[1]];
+            newObj.GetComponent<MoveObs>().SetSpeed(speed);
+        }
+    }
+}
---
+++ Assets/Src/Scripts/MoveObs.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MoveObs : MonoBehaviour
+{
+    [SerializeField] Vector3 dir;
+    private float speed = 0f;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void SetSpeed(float newSpeed)
+    {
+        speed = newSpeed;
+        return;
+    }
+
+    private void Update()
+    {
+        rb.AddForce(dir * speed * Time.deltaTime);
+    }
+}
